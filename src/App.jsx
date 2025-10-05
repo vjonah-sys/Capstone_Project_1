a5b3b29bbf0d401c917c5947826c05663299617a
vjonah-sys <vanjonah045@gmail.com>
2025-10-06 00:55:20 +0100

    Creating the Capstone structure

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-blue-400">Tailwind is Working 🎉</h1>
+    </div>
+  );
+}
